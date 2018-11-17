77ef5109a07f591579cd843c8be6946f924f578a
Taylor Lewis <tlew@Taylors-MacBook-Pro.local>
2018-11-17 10:31:43 -0600

    first commit

---
+++ app/data/friends.js
@@ -0,0 +1,72 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the reserved tables.
+// Initially we just set it equal to a "dummy" customer.
+// But you could have it be an empty array as well.
+// ===============================================================================
+
+var friends = [
+  {
+    name: "Earn",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: "Paper Boy",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: "Tracy",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: "Darius",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: "Vanessa",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: "Dennis",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: "Frank",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: "Dee",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: "Mac",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: "Charlie",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [1, 3, 5, 2, 1, 3, 4, 4, 4, 1]
+  }
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,88 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+var friends = require("../data/friends");
+var path = require("path");
+
+
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/friends", function(req, res) {
+    console.log("FriendFinder API Loading...");
+    res.json(friends);
+  });
+
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // Then the server saves the data to the tableData array)
+  // ---------------------------------------------------------------------------
+
+  app.post("/api/friends", function(req, res) {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    // req.body is available since we're using the body parsing middleware
+
+    // variables for adding new friend to database server 
+    var addFriend = req.body;
+    var addedScore = addFriend.scores; 
+    var total = 0;
+    var friendMatch = 1000; // set value to big number
+    var friendName = "";
+    var friendPic = "";
+    //  
+
+    // use a for loop to shuffle through friends data array in module exports
+    for( var i = 0; i < friends.length; i++){
+      //
+      total = 0;
+
+      // create second for loop for calculation of intScore of new friend to actual data stored in array
+      for( var t = 0; t < addedScore.length; t++){
+        var totalDifference = Math.abs(addedScore[t] - friends[i].scores[t]);
+        total += totalDifference;
+      }
+      if(total < friendMatch){
+        friendMatch = total;
+        console.log("Best Match =", friends[i]);
+        console.log("Best Match =", friends[i]);
+        console.log("Best Match =", friends[i]);
+
+        friendMatch = total;
+        friendName = friends[i].name;
+        friendPic = friends[i].photo;
+    
+      }
+    }
+    friends.push(addFriend);
+    res.json({status: "OK", name: friendName, photo: friendPic});
+  })
+    
+
+  // ---------------------------------------------------------------------------
+  // I added this below code so you could clear out the table while working with the functionality.
+  // Don"t worry about it!
+
+  app.post("/api/clear", function(req, res) {
+    // Empty out the arrays of data
+    tableData.length = [];
+    waitListData.length = [];
+
+    res.json({ ok: true });
+  })
+}
